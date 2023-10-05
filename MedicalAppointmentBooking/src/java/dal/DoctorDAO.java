06d8fd86ae80ee2d495f52427b09803b4920a869
Mon1712 <133070020+Mon1712@users.noreply.github.com>
2023-10-05 12:38:41 +0700

    listv3

--- MedicalAppointmentBooking/src/java/dal/DoctorDAO.java
+++ MedicalAppointmentBooking/src/java/dal/DoctorDAO.java
@@ -78,8 +78,10 @@ public class DoctorDAO {
         List<Doctor> doctorList = new ArrayList<>();
 
         String sql = "select * from doctors d \n"
-                + "			inner join user_account u on d.user_id = u.user_id \n"
-                + "			inner join speciality s on s.speciality_id = d.speciality_id\n";
+                + "            inner join user_account u on d.user_id = u.user_id \n"
+                + "            inner join  speciality s on s.speciality_id = d.speciality_id\n"
+                + "            inner join services ser on ser.service_id = d.service_id"
+                + "            where u.full_name LIKE ?";
 
         Connection connection = null;
         try {
@@ -99,7 +101,7 @@ public class DoctorDAO {
                 String position = rs.getString("doctor_position");
                 String speciality = rs.getString("speName");
                 String description = rs.getString("doctor_description");
-          
+
                 Doctor d = new Doctor(doctorId, speciality, position, description, username, email, name, gender, phone, image, status);
                 doctorList.add(d);
             }
