a70059abb722be5b68f06ca7f4ff05a618f998cf
Quyenlnhe173212 <135082152+Quyenlnhe173212@users.noreply.github.com>
2023-10-22 20:03:51 +0700

    zzz

--- MedicalAppointmentBooking/src/java/dal/RoleDAO.java
+++ MedicalAppointmentBooking/src/java/dal/RoleDAO.java
@@ -4,10 +4,40 @@
  */
 package dal;
 
+import dbContext.DBConnection;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import model.Role;
+import model.UserAccount;
+import utils.ImageProcessing;
+
 /**
  *
  * @author Admin
  */
 public class RoleDAO {
-    
+      DBConnection dbc = new DBConnection();
+       public ArrayList<Role> getListRole() {
+        PreparedStatement stm = null;
+        Connection connection = null;
+        ResultSet rs = null;
+        ArrayList<Role> list = new ArrayList<>();
+        String sql = "SELECT r.* FROM user_role r ";
+        try {
+            connection = dbc.getConnection();
+            stm = connection.prepareStatement(sql);
+            rs = stm.executeQuery();
+            while (rs.next()) {
+                Role r = new Role(rs.getInt("role_id"),rs.getString("role_name"));
+                list.add(r);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        return list;
+    }
 }
