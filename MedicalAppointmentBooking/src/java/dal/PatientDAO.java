c4b2d7e657b10525b7493626f920cfa5ac9a779b
PhuongNLTHE176328 <phuongnlthe176328@fpt.edu.vn>
2023-10-26 00:28:59 +0700

    s

--- MedicalAppointmentBooking/src/java/dal/PatientDAO.java
+++ MedicalAppointmentBooking/src/java/dal/PatientDAO.java
@@ -354,4 +354,8 @@ PreparedStatement ps = null;
         return null;
     }
 
+    public List<Patient> getPatientByDoctorIdSortZA(int docId) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
