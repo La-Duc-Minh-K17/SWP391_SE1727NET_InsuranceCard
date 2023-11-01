56584a469fb1b757fcc4119796157f3f6202981a
Quyenlnhe173212 <135082152+Quyenlnhe173212@users.noreply.github.com>
2023-11-01 11:45:23 +0700

    Update AccountAdd.java

--- MedicalAppointmentBooking/src/java/controller/admin/AccountAdd.java
+++ MedicalAppointmentBooking/src/java/controller/admin/AccountAdd.java
@@ -96,9 +96,9 @@ public class AccountAdd extends HttpServlet {
         UserAccount user = new UserAccount(username, "123456@", email, fullname, Integer.valueOf(gender), phone, TimeUtil.dateConverter(dob), address, null, null, 1, new Role(Integer.valueOf(role)));
         if (uDAO.isAccountExisted(user)) {
             request.setAttribute("error", "Account has existed !");
-        } else { 
+        } else {
             uDAO.addUserAccount(user);
-            request.setAttribute("success", "Account added successfully with password 123456@!");
+            request.setAttribute("success", "Account has been added successfully with password 123456@!");
         }
         request.getRequestDispatcher("frontend/view/admin/accountadd.jsp").forward(request, response);
     }
